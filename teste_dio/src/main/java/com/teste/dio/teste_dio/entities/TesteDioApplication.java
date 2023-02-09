32c1ee1ff14a522e7977d4be0e623c3de54d2fab
jeffer123github <jeffer123github@gmail.com>
2023-02-09 12:13:47 -0300

    criado api REST com banco relacional H2 pera desafil de teste da DIO

---
+++ teste_dio/src/main/java/com/teste/dio/teste_dio/entities/TesteDioApplication.java
@@ -0,0 +1,13 @@
+package com.teste.dio.teste_dio.entities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteDioApplication.class, args);
+	}
+
+}
---
+++ teste_dio/src/test/java/com/teste/dio/teste_dio/entities/TesteDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.dio.teste_dio.entities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
